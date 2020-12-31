32fa0b4a982e2139f15b336136ac864724514ae1
Matt Smith <mcsmith.inbox@gmail.com>
2020-12-31 18:11:43 -0500

    Adds hot reload support for Vagrant

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  configureWebpack: {
+    devServer: {
+      public: '0.0.0.0:8080', // Allows hot reload on host
+      disableHostCheck: true, // When not the host machine, need to disable the check and get the poll to check for file edits
+      watchOptions: {
+        poll: true
+      }
+    }
+  }
 }
