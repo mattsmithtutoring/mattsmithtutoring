d9edbe84fd9b33569b1de2b4a57a9b845ef47c93
Matt Smith <mcsmith.inbox@gmail.com>
2021-01-02 00:57:27 -0500

    Adds header component

---
+++ src/components/McsHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="text-h2 font-weight-bold text-center pt-4">
+      <slot></slot>
+    </div>
+    <div class="text-h6 text-center pb-2" v-if="subheader">
+      {{ subheader }}
+    </div>
+    <div class="pb-4" v-else></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    subheader: String
+  }
+}
+</script>
