715625a0706155e449ec11683a194b8da620e6d6
Matt Smith <mcsmith.inbox@gmail.com>
2021-01-02 00:57:16 -0500

    Adds blurb component

---
+++ src/components/Blurb.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-row>
+    <v-col cols="4" v-if="!right">
+      <v-card
+        height="8rem"
+        :color="color"
+        dark
+        class="d-flex justify-center align-center"
+      >
+        <v-card-title class="text-h3" style="white-space: pre-line">
+          {{ header }}
+        </v-card-title>
+      </v-card>
+    </v-col>
+    <v-col cols="8">
+      <v-card height="8rem" class="d-flex align-center">
+        <v-card-text class="text--primary text-h6">
+          <slot></slot>
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="4" v-if="right">
+      <v-card
+        height="8rem"
+        :color="color"
+        dark
+        class="d-flex justify-center align-center"
+      >
+        <v-card-title class="text-h3" style="white-space: pre-line">
+          {{ header }}
+        </v-card-title>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'Blurb',
+  props: {
+    color: String,
+    header: String,
+    right: Boolean
+  }
+}
+</script>
