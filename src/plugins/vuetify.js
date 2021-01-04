63b743eaf02c9f48e02515b07c730b74a53c5be4
Matt Smith <mcsmith.inbox@gmail.com>
2021-01-03 21:26:32 -0500

    Sets up theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,39 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
-
+import colors from 'vuetify/lib/util/colors'
 Vue.use(Vuetify)
 
-export default new Vuetify({
-})
+const vuetifyOptions = {
+  theme: {
+    themes: {
+      light: {
+        primary: colors.deepOrange,
+        secondary: colors.blueGrey,
+        accent: colors.cyan,
+        success: colors.green,
+        error: colors.red,
+        info: colors.deepPurple,
+        warning: colors.amber,
+        item1: colors.purple.lighten1,
+        item2: colors.yellow.darken3,
+        item3: colors.red.darken2,
+        item4: colors.cyan.darken1
+      },
+      dark: {
+        primary: colors.deepOrange,
+        secondary: colors.blueGrey,
+        accent: colors.cyan,
+        success: colors.green,
+        error: colors.red,
+        info: colors.deepPurple,
+        warning: colors.amber,
+        item1: colors.purple.lighten1,
+        item2: colors.yellow.darken3,
+        item3: colors.red.darken2,
+        item4: colors.cyan.darken1
+      }
+    }
+  }
+}
+
+export default new Vuetify(vuetifyOptions)
