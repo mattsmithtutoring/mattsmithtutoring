6885fca5a5a390b7f048657b0d0afdbd97f60551
Matt Smith <mcsmith.inbox@gmail.com>
2021-01-03 22:12:24 -0500

    Adds component to book times

---
+++ src/components/TimeBooker.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <v-calendar color="primary">
+      <template v-slot:day="{ date }">{{ date }}</template>
+      <!-- <template v-slot:day-label="{ day }">
+        <div class="blue">{{ day }}</div>
+      </template> -->
+    </v-calendar>
+  </div>
+</template>
