1f2a53d99b52225377acd200fd28b19225c5f9b7
Matt Smith <mcsmith.inbox@gmail.com>
2021-01-03 22:12:11 -0500

    Adds page to test new components

---
+++ src/views/TestingGround.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <time-booker></time-booker>
+  </div>
+</template>
+
+<script>
+import TimeBooker from '../components/TimeBooker.vue'
+export default {
+  components: {
+    TimeBooker
+  },
+  name: 'TestingGround'
+}
+</script>
