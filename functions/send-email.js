921e0468f32ad36b98ec3ce47ff7145c7971051d
Matt Smith <mcsmith.inbox@gmail.com>
2021-01-31 21:12:37 -0500

    Adds Netlify function to send email through sendgrid

--- functions/send-email.js
+++ functions/send-email.js
@@ -1,21 +1,61 @@
-require('dotenv').config()
-// const { EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, EMAILJS_USER_ID } = process.env
-// const emailjs = require('emailjs-com')
+// const sendgrid = require('@sendgrid/mail')
+const { SENDGRID_API_KEY, SENDGRID_TO_EMAIL, SENDGRID_FROM_EMAIL } = process.env
 
-exports.handler = async function(event, context) {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed', headers: { Allow: 'POST' } }
+exports.handler = async function (event, context, callback) {
+  const formData = JSON.parse(event.body)
+  // sendgrid.setApiKey(SENDGRID_API_KEY)
+
+  const data = {
+    to: SENDGRID_TO_EMAIL,
+    from: SENDGRID_FROM_EMAIL,
+    subject: `New inquiry from ${formData.student.firstname} ${formData.student.lastname}`,
+    html: `
+Student:
+<ul>
+  <li>First name: ${formData.student.firstname}</li>
+  <li>Last name: ${formData.student.lastname}</li>
+  <li>Age: ${formData.student.age}</li>
+  <li>Email: ${formData.student.email}</li>
+</ul>
+
+Parent:
+<ul>
+  <li>First name: ${formData.parent.firstname}</li>
+  <li>Last name: ${formData.parent.lastname}</li>
+  <li>Email: ${formData.parent.email}</li>
+</ul>
+
+Subject:
+<ul>
+  <li>Subject: ${formData.subject}</li>
+  <li>Other subject description: ${formData.otherSubjectDescription}</li>
+  <li>Course: ${formData.course}</li>
+  <li>Other course description: ${formData.otherCourseDescription}</li>
+</ul>
+
+Additional Notes/Comments:
+<blockquote>${formData.additionalNotes}</blockquote>
+`
   }
 
   return {
     statusCode: 200,
-    body: event.body
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ hello: 'world', apikey: SENDGRID_API_KEY, email: SENDGRID_TO_EMAIL })
   }
-  // return emailjs.sendForm(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, event.body, EMAILJS_USER_ID).then(() => ({
-  //   statusCode: 200,
-  //   body: 'success'
-  // })).catch(error => ({
-  //   statusCode: 422,
-  //   body: `Error: ${error}`
-  // }))
+
+  // try {
+  //   await sendgrid.send(data)
+  //   return {
+  //     statusCode: 200,
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify({ success: true })
+  //   }
+  // } catch (error) {
+  //   return {
+  //     statusCode: error.code,
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify({ success: false, error: error.message })
+  //   }
+  // }
 }
