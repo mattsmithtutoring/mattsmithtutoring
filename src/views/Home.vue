b58474b014a1083207e0c8b6d88525a27194fbf6
Matt Smith <mcsmith.inbox@gmail.com>
2021-02-03 22:14:28 -0500

    Adds scrollable property to dialog

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <v-app-bar app dark color="secondary">
       <v-toolbar-title>Matt Smith Tutoring</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-dialog v-model="dialog" width="900" persistent>
+      <v-dialog v-model="dialog" width="900" persistent scrollable>
         <template v-slot:activator="{ on, attrs }">
           <v-btn color="primary" v-bind="attrs" v-on="on">BOOK NOW</v-btn>
         </template>
@@ -19,7 +19,9 @@
             Hi! I'm Mr. Smith, a Princeton University-trained mathematician and certified math teacher who helps
             students reach their full potential in the subjects they struggle with the most. My approach is analytical:
             break down a problem, whether it be solving an equation or translating a sentence, into pieces and build on
-            the fundamentals to piece the solution together. <br /><br />
+            the fundamentals to piece the solution together.
+            <br />
+            <br />
             I'll meet you where you're at and help you reach where you need to be. Through consistent effort, you'll
             build confidence and overcome the struggle. See below for more details on my teaching philosophy. Or click
             "Book Now" in the top right to book a free introductory session today to get started on your journey.
@@ -101,13 +103,13 @@
         </v-row>
         <blurb header="Perseverance" color="item1">
           By embracing and working through difficult problems, we learn to
-          <span class="item1--text font-weight-bold">persevere</span> through the challenge and not fear solving new
-          problems with unknown answers.
+          <span class="item1--text font-weight-bold">persevere</span>
+          through the challenge and not fear solving new problems with unknown answers.
         </blurb>
         <blurb header="Curiosity" color="item2" right>
           As we become more comfortable with the subjects that we struggle with, our natural
-          <span class="item2--text font-weight-bold">curiosity</span> is unlocked and puts us in a state where we are
-          ready to learn and discover new things.
+          <span class="item2--text font-weight-bold">curiosity</span>
+          is unlocked and puts us in a state where we are ready to learn and discover new things.
         </blurb>
         <blurb
           header="Analysis +
@@ -115,14 +117,15 @@
           color="item3"
         >
           Learning to properly
-          <span class="item3--text font-weight-bold">analyze</span> a given problem trains our brains to be
-          detail-oriented, while learning to <span class="item3--text font-weight-bold">synthesize</span> the results
-          helps us to keep sight of the big picture in the process.
+          <span class="item3--text font-weight-bold">analyze</span>
+          a given problem trains our brains to be detail-oriented, while learning to
+          <span class="item3--text font-weight-bold">synthesize</span>
+          the results helps us to keep sight of the big picture in the process.
         </blurb>
         <blurb header="Creativity" color="item4" right>
           We will encounter obstacles in solving any kind of problem, which gives us the opportunity to learn how to
-          <span class="item4--text font-weight-bold">creatively</span> navigate around those obstacles by reducing new
-          problems to things we already know.
+          <span class="item4--text font-weight-bold">creatively</span>
+          navigate around those obstacles by reducing new problems to things we already know.
         </blurb>
       </v-container>
     </div>
@@ -134,9 +137,9 @@
             Tutoring is most effective when done on a regular basis. The repeated act of embracing challenges in a safe
             environment helps to build confidence with the subject and overcome the fear of failure. Each of these
             enhances the other in a feedback loop I call the
-            <span class="font-weight-bold">Tutoring Cycle</span>. Altogether, the cycle helps us reach the ultimate goal
-            of sharpening the mind overall and sets us up to be successful in any challenges we face (which, naturally,
-            will also lead to better grades in school).
+            <span class="font-weight-bold">Tutoring Cycle</span>
+            . Altogether, the cycle helps us reach the ultimate goal of sharpening the mind overall and sets us up to be
+            successful in any challenges we face (which, naturally, will also lead to better grades in school).
           </v-col>
         </v-row>
         <tutor-cycle></tutor-cycle>
@@ -151,13 +154,14 @@
         </v-row>
         <v-row>
           <v-col cols="6" offset="3">
-            <v-btn block x-large dark color="primary" width="100%" class="text-h6"> BOOK NOW </v-btn>
+            <v-btn block x-large dark color="primary" width="100%" class="text-h6">BOOK NOW</v-btn>
           </v-col>
         </v-row>
       </v-container>
     </div>
     <v-footer color="secondary" dark class="text-caption text-md-body-1">
-      Contact me: <a href="#" class="white--text pl-2">mattsmith@mattsmithtutoring.com</a>
+      Contact me:
+      <a href="#" class="white--text pl-2">mattsmith@mattsmithtutoring.com</a>
       <v-spacer></v-spacer>
       &copy; 2021 Matt Smith Tutoring
     </v-footer>
